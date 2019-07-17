f1a556bb022503c10b77efe24d665e7bc744d1cd
Federico Cergol <federico.cergol@gmail.com>
2019-07-18 00:16:56 +0200

    dev: added labels for the parser

--- parser.py
+++ parser.py
@@ -9,6 +9,19 @@ registers = {
     "D": 0b11
 }
 
+def parseValue(value):
+    try:
+        val = int(value, 0)
+        assert val < 256 and val > -128
+        return val % 256
+    except ValueError:
+        if value.startswith(".") or value.startswith(":"):
+            return value
+        else:
+            print(f"Unknown value: {value}")
+
+labels = {}
+
 out = []
 with open(fname, "r") as f:
     for line in f.readlines():
@@ -20,6 +33,12 @@ with open(fname, "r") as f:
         
         # remove inline comments
         line = line.split("#")[0].strip()
+
+        # handle label definition
+        if line.startswith(".") or line.startswith(":"):
+            assert line not in labels
+            labels[line] = len(out)
+            continue
 
         instruction = line.split(' ')[0]
         if instruction == "MV":
@@ -46,7 +65,7 @@ with open(fname, "r") as f:
         elif instruction == "LI":
             argvector = "".join(line.split(" ")[1:])
             to = argvector.split(",")[0].strip()
-            value = int(argvector.split(",")[1].strip(), 0)
+            value = parseValue(argvector.split(",")[1].strip())
             out.append((0b00_10_00 << 2) | registers[to])
             out.append(value)
 
@@ -77,7 +96,7 @@ with open(fname, "r") as f:
         elif instruction == "ADDI":
             argvector = "".join(line.split(" ")[1:])
             to = argvector.split(",")[0].strip()
-            value = int(argvector.split(",")[1].strip(), 0)
+            value = parseValue(argvector.split(",")[1].strip())
             out.append((0b10_00_00 << 2) | registers[to])
             out.append(value)
 
@@ -94,30 +113,36 @@ with open(fname, "r") as f:
             out.append((0b10_00_11 << 2) | registers[to])
 
         elif instruction == "JMP":
-            argvector = "".join(line.split(" ")[1:])
-            h = int(argvector.split(",")[0].strip(), 0)
-            l = int(argvector.split(",")[1].strip(), 0)
-            out.append(0b11_00_00_00)
-            out.append(h)
-            out.append(l)
+            argvector = "".join(line.split(" ")[1:]).strip()
+            if argvector.startswith(":"):
+                # abs label
+                out.append(0b11_00_00_00)
+                out.append(f"{argvector}H")
+                out.append(f"{argvector}L")
+            else:
+                h = parseValue(argvector.split(",")[0].strip())
+                l = parseValue(argvector.split(",")[1].strip())
+                out.append(0b11_00_00_00)
+                out.append(h)
+                out.append(l)
         
         elif instruction == "JCR":
-            value = int(line.split(" ")[1].strip(), 0)
+            value = parseValue(line.split(" ")[1].strip())
             out.append(0b11_00_01_00)
             out.append(value)
 
         elif instruction == "JZR":
-            value = int(line.split(" ")[1].strip(), 0)
+            value = parseValue(line.split(" ")[1].strip())
             out.append(0b11_00_01_01)
             out.append(value)
 
         elif instruction == "JNR":
-            value = int(line.split(" ")[1].strip(), 0)
+            value = parseValue(line.split(" ")[1].strip())
             out.append(0b11_00_01_10)
             out.append(value)
 
         elif instruction == "JLTR":
-            value = int(line.split(" ")[1].strip(), 0)
+            value = parseValue(line.split(" ")[1].strip())
             out.append(0b11_00_01_11)
             out.append(value)
 
@@ -129,7 +154,28 @@ with open(fname, "r") as f:
 
 with open("ram.img", "w") as f:
     f.write("v2.0 raw\n")
-    for value in out:
-        code = hex(value)[2:]
-        f.write(f"{code}\n")
-        
+    for idx, value in enumerate(out):
+        try:
+            code = hex(value)[2:]
+            f.write(f"{code}\n")
+        except TypeError:
+            # its a label
+            if value.startswith("."):
+                # relative label
+                assert value in labels
+                offset = (labels[value] - idx)
+                assert offset > -128 and offset < 128
+                offset = offset-1 % 256
+                f.write(f"{offset}\n")
+            elif value.startswith(":"):
+                assert value[:-1] in labels
+                if value[-1] == "L":
+                    code = hex(labels[value[:-1]] & 0b11_11_11_11)[2:]
+                    f.write(f"{code}\n")
+                elif value[-1] == "H":
+                    code = hex((labels[value[:-1]] & (0b11_11_11_11 << 8)) >> 8)[2:]
+                    f.write(f"{code}\n")
+                else:
+                    print(f"Error: absolute label {value}")
+            else:
+                print(f"Unknown label type: {value}")
